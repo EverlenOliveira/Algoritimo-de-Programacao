d7a96c7e195b9ad637c34db5dcb60185a7297070
EverlenOliveira <oevasousa@gmail.com>
2025-08-26 13:43:30 -0300

    Função int + input para somar

---
+++ soma.py
@@ -0,0 +1,10 @@
+num1 = int(input("Digite um numero:"))
+num2 = int(input("Digite outro numero:"))
+soma = num1 + num2
+print(soma)
+
+print("=====================================================")
+
+num1=int(input("Digite um numero:"))
+num2=int(input("Digite outro numero:"))
+print('A soma dos dois numeros é {}'.format(num1+num2))
